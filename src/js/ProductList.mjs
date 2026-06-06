9aeb5bcc6c8617ad178eb5baaa9c1c5d511131b1
luyinga <lumbaleluyinga2018@gmail.com>
2026-06-06 19:11:37 +0200

    Changing quantity optionability to change the quantity of product desired

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -1,5 +1,16 @@
 import { renderListWithTemplate } from "./utils.mjs";
 
+export function discountBadgeTemplate(product) {
+  const isDiscounted = product.FinalPrice < product.SuggestedRetailPrice;
+  if (isDiscounted) {
+    const savings = (product.SuggestedRetailPrice - product.FinalPrice).toFixed(
+      2,
+    );
+    return `<span class="discount-badge">Save $${savings}!!!</span>`;
+  }
+  return "";
+}
+
 function productCardTemplate(product) {
   return `
     <li class="product-card">
@@ -21,17 +32,41 @@ export default class ProductList {
   }
 
   async init() {
-    const list = await this.dataSource.getData();
+    const list = await this.dataSource.getData(this.category);
+
+    this.originalList = list;
+
     this.renderList(list);
+
+    const sortSelect = document.querySelector("#sort-products");
+    if (!sortSelect) {
+      return;
+    }
+
+    sortSelect.addEventListener("change", (e) => {
+      const sortedList = [...this.originalList];
+
+      switch (e.target.value) {
+        case "name-asc":
+          sortedList.sort((a, b) => a.Name.localeCompare(b.Name));
+          break;
+        case "name-desc":
+          sortedList.sort((a, b) => b.Name.localeCompare(a.Name));
+          break;
+        case "price-asc":
+          sortedList.sort((a, b) => a.FinalPrice - b.FinalPrice);
+          break;
+        case "price-desc":
+          sortedList.sort((a, b) => b.FinalPrice - a.FinalPrice);
+          break;
+      }
+
+      this.renderList(sortedList);
+    });
   }
 
   renderList(list) {
-    // const htmlStrings = list.map(productCardTemplate);
-    // this.listElement.insertAdjacentHTML("afterbegin", htmlStrings.join(""));
-
-    // apply use new utility function instead of the commented code above
     renderListWithTemplate(productCardTemplate, this.listElement, list);
-
   }
-
 }
+
