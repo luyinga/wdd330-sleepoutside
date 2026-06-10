313484e9b1883ccbb5e12a85cb74fe0ce3b2e8b0
luyinga <lumbaleluyinga2018@gmail.com>
2026-06-10 15:15:37 +0200

    Fixed bug

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,7 +4,7 @@ import { loadHeaderFooter, itemsInCart } from "./utils.mjs";
 loadHeaderFooter();
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 
