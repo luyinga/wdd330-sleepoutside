29551ebda7608e8106620cb9ec7028c547ce3b5f
luyinga <lumbaleluyinga2018@gmail.com>
2026-06-10 14:42:07 +0200

    Merge branch 'main' of https://github.com/luyinga/wdd330-sleepoutside

--- src/js/cart.js
+++ src/js/cart.js
@@ -110,3 +110,20 @@ function cartTotal(items) {
     hideTotal.classList.add("cart-footer-hide")
   }
 }
+
+//Add a Call to action-register with site
+document.addEventListener("DOMContentLoaded", () => {
+  const modal = document.getElementById("register-modal");
+  const closeBtn = document.getElementById("close-modal");
+
+  const hasSeenModal = localStorage.getItem("registerModalShown");
+
+  if (!hasSeenModal) {
+    modal.classList.remove("hidden");
+    localStorage.setItem("registerModalShown", "true");
+  }
+
+  closeBtn?.addEventListener("click", () => {
+    modal.classList.add("hidden");
+  });
+});
